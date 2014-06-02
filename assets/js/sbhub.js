bb484728d20bf9a1dc6fbf771373b66e1ea1d327
DaveCaraway <dave@fogmine.com>
2014-06-02 15:55:41 -0400

    Convert to use Angular ngResource and added search
    Converted to using the ngResource for REST services and then added default query parameter (api_key) and added further filtering.
    
    FIXES https://github.com/18F/afsmallbiz/issues/44

--- assets/js/sbhub.js
+++ assets/js/sbhub.js
@@ -1,11 +1,17 @@
-angular.module('sbhub', []).controller('oppsCtrl', function ($scope, $http) {
-    $scope.opps = {};
+angular.module('sbhub', ['ngResource']).controller('oppsCtrl', function ($scope, $http, $resource) {
     $http.defaults.useXDomain = true;
-    $http.get('http://api.data.gov/gsa/fbopen/v0/opps?api_key=8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7')
-        .success(function(data) {
-            $scope.opps.results = data;
-        }).error(function(data, status){
+    $scope.Opp = $resource('http://api.data.gov/gsa/fbopen/v0/opps', {'api_key': '8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7'});
+
+    $scope.getOpps = function(filter){
+        filter = filter || {};
+        $scope.Opp.get(filter, function(data){
+            $scope.opps = data;
+        }, function(data, status){
             console.log('Request failed:'+status);
         });
+    };
+
+    //On page load, get opportunities with no filtering.
+    $scope.getOpps();
 
 });
