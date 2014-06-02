e6bec751ab4f9dc24f26d8008327eee9de73b2c3
DaveCaraway <dave@fogmine.com>
2014-06-02 13:31:38 -0400

    Angular with Stubbed Filtering Form
    Added the angularjs integration with stubbed filtering form. The form is there applying filters but doesn't actually do filtering or get results yet.
    
    SEE https://github.com/18F/afsmallbiz/issues/44

--- assets/js/sbhub.js
+++ assets/js/sbhub.js
@@ -1,14 +1,11 @@
-angular.module('sbhub', ['ngResource']).controller('appCtrl', function ($scope, $http) {
+angular.module('sbhub', []).controller('oppsCtrl', function ($scope, $http) {
+    $scope.opps = {};
     $http.defaults.useXDomain = true;
-
-    $scope.getOpps = function() {
-        $http.get('http://api.data.gov/gsa/fbopen/v0/opps')
-            .success(function(data) {
-                $scope.opps = data;
-                console.log('Opps query performed'); 
-            }).error(function(data, status){
-                console.log('Request failed:'+status);
-            });
-    };
+    $http.get('http://api.data.gov/gsa/fbopen/v0/opps?api_key=8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7')
+        .success(function(data) {
+            $scope.opps.results = data;
+        }).error(function(data, status){
+            console.log('Request failed:'+status);
+        });
 
 });
