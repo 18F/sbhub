bf7d170b005147850bf193f67bd3777dc9c31437
DaveCaraway <dave@fogmine.com>
2014-06-03 12:06:32 -0400

    Added filter stubs and result count

--- assets/js/sbhub.js
+++ assets/js/sbhub.js
@@ -1,10 +1,10 @@
 angular.module('sbhub', ['ngResource']).controller('oppsCtrl', function ($scope, $http, $resource) {
     $http.defaults.useXDomain = true;
-    $scope.Opp = $resource('http://api.data.gov/gsa/fbopen/v0/opps', {'api_key': '8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7'});
+    var Opp = $resource('http://api.data.gov/gsa/fbopen/v0/opps', {'api_key': '8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7'});
 
     $scope.getOpps = function(filter){
         filter = filter || {};
-        $scope.Opp.get(filter, function(data){
+        Opp.get(filter, function(data){
             $scope.opps = data;
         }, function(data, status){
             console.log('Request failed:'+status);
