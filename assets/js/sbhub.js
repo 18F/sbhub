8d207f143521b05008f1b8f5b2c072d5b8036017
DaveCaraway <dave@fogmine.com>
2014-06-03 17:59:56 -0400

    Added higher-contrast solicitation title, implementing jquery shorten on the description but not quite there yet

--- assets/js/sbhub.js
+++ assets/js/sbhub.js
@@ -1,4 +1,6 @@
-angular.module('sbhub', ['ngResource']).controller('oppsCtrl', function ($scope, $http, $resource) {
+var sbhub = angular.module('sbhub', ['ngResource']);
+
+sbhub.controller('oppsCtrl', function ($scope, $http, $resource) {
     $http.defaults.useXDomain = true;
     var Opp = $resource('http://api.data.gov/gsa/fbopen/v0/opps', {'api_key': '8l3xbEmsQMq7AG7mXoSy3IuJAqehmWGRC754Otx7'});
 
@@ -6,6 +8,7 @@ angular.module('sbhub', ['ngResource']).controller('oppsCtrl', function ($scope,
         filter = filter || {};
         Opp.get(filter, function(data){
             $scope.opps = data;
+            $scope.$broadcast('dataloaded');
         }, function(data, status){
             console.log('Request failed:'+status);
         });
@@ -15,3 +18,15 @@ angular.module('sbhub', ['ngResource']).controller('oppsCtrl', function ($scope,
     $scope.getOpps();
 
 });
+
+sbhub.directive('shorten', ['$timeout', function ($timeout) {
+    return {
+        link: function ($scope, element, attrs) {
+            $scope.$on('dataloaded', function () {
+                    $timeout(function () { // You might need this timeout to be sure its run after DOM render.
+                     alert('hello!');
+                 }, 0, false);
+                })
+        }
+    };
+}]);
